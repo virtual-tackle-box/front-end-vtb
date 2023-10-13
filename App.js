3612e187c25a1d3c80f96fb5535e6d1862dd10e6
Banjamin Hatch <banjaminhatch@gmail.com>
2023-10-13 10:03:08 -0600

    Delete login file

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-import Login from './components/Login';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Login/>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
