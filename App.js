f458b1bfc2ee922f0ae01228e8dbf37c0b970dbc
Banjamin Hatch <banjaminhatch@gmail.com>
2023-10-13 15:25:35 -0600

    add testIDs to inputs and button

--- App.js
+++ App.js
@@ -1,17 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Image, SafeAreaView } from 'react-native';
 import Login from './components/Login/Login.jsx';
-import styles from './components/App/AppStyles.jsx';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
 export default function App() {
-  
-  const handleLogin = (name,password) => {
-   
-}
+
   
   
   return (
@@ -19,12 +13,17 @@ export default function App() {
       <Stack.Navigator initialRouteName='Login'>
           <Stack.Screen 
             name='Login' 
+            options={{ 
+              headerShown: false,
+            }}
             component={Login}
-            options={{ headerShown: false }}
             />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+  
+  
+  
           
 
