93a5101a2866fbd796d15c5c9394d72e55a78114
Banjamin Hatch <banjaminhatch@gmail.com>
2023-10-13 17:45:54 -0600

    Add dashboard component to stack navigation

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import Login from './components/Login/Login';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import Dashboard from './components/DashBoard/Dashboard';
 
 const Stack = createStackNavigator();
 
@@ -17,6 +18,13 @@ export default function App() {
               headerShown: false,
             }}
             component={Login}
+            />
+            <Stack.Screen 
+            name='Dashboard' 
+            options={{ 
+              headerShown: false,
+            }}
+            component={Dashboard}
             />
       </Stack.Navigator>
     </NavigationContainer>
