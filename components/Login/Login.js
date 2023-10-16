b9346454725fecdb6d3c781f761c39c42d1ce18d
Banjamin Hatch <banjaminhatch@gmail.com>
2023-10-16 15:46:02 -0600

    Fixes import of dashboard

--- components/Login/Login.js
+++ components/Login/Login.js
@@ -3,7 +3,7 @@ import { View, Text, TextInput, Button, Image } from "react-native";
 import { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import styles from './LoginStyles'
-import MapView from 'react-native-maps'
+
 
 function Login(){
 
@@ -55,9 +55,6 @@ function Login(){
                         title="Login" 
                         onPress={() => handleLogin(name,password)}/>
                 </View>
-            </View>
-            <View style={styles.mapcontainer}>
-                <MapView styles={styles.map}/>
             </View>
         </View>
     )
