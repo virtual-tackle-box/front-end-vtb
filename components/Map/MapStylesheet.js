62d15ea8394dd14bf5d6a731890cebba119a347b
Banjamin Hatch <banjaminhatch@gmail.com>
2023-10-16 13:45:05 -0600

    Separates stylesheet for map

---
+++ components/Map/MapStylesheet.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Dimensions } from "react-native-web";
+
+export const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	map: {
+		width: '100%',
+		height: '100%',
+	},
+});
