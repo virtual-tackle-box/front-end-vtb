d48124cb03d0b584e243fbd00bd1d19709284505
Banjamin Hatch <banjaminhatch@gmail.com>
2023-10-16 13:44:37 -0600

    Places map on the dashboard

--- components/Map/Map.js
+++ components/Map/Map.js
@@ -1,43 +1,46 @@
-import React from 'react';
-import MapView  from 'react-native-maps';
-import {Marker} from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import React from "react";
+
+import { StyleSheet, Text, View, Dimensions, Platform } from "react-native";
+import { styles } from "./MapStylesheet";
 
 export default function TestMap() {
+	let Marker;
+	let MapView;
+	let onWeb = false;
+	if (Platform.OS !== "web") {
+		MapView = require("react-native-maps").default;
+		Marker = require("react-native-maps").Marker;
+	} else {
+		onWeb = true;
+	}
 
-    const testPin = {
-       coordinates: {
-        latitude: 40.428712,
-        longitude: -105.033767,
-        latitudeDelta: 0.01,
-        longitudeDelta: 0.01,
-       },
-       title:'Bass',
-       description:'3lbs'
-    }
+	const testPin = {
+		coordinates: {
+			latitude: 40.428712,
+			longitude: -105.033767,
+			latitudeDelta: 0.01,
+			longitudeDelta: 0.01,
+		},
+		title: "Bass",
+		description: "3lbs",
+	};
 
-  return (
-    <View style={styles.container}>
-      <MapView style={styles.map}>
-        <Marker 
-            coordinate={testPin.coordinates}
-            title={testPin.title}
-            description={testPin.description}
-        />
-      </MapView>
-    </View>
-  );
+	const mapView =
+		onWeb === false ? (
+			<View style={styles.container}>
+				<MapView style={styles.map}>
+					<Marker
+						coordinate={testPin.coordinates}
+						title={testPin.title}
+						description={testPin.description}
+					/>
+				</MapView>
+			</View>
+		) : (
+			<View style={styles.container}>
+				<Text>Map not supported in Web Mode!</Text>
+			</View>
+		);
+
+	return mapView;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
-});
