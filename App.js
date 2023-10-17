fa071765f59c5ac7539ef0577fe8369ddafa75d1
Joshua Bennett <joshbennett793@yahoo.com>
2023-10-17 15:46:34 -0500

    Add AddCatch component to stack navigation

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Dashboard from './components/Dashboard/Dashboard.js';
 import AddCatch from './components/AddCatch/AddCatch';
-// import Dashboard from './components/Dashboard/Dashboard';
 
 const Stack = createStackNavigator();
 
@@ -11,26 +10,28 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Login'>
-          <Stack.Screen 
-            name='Login' 
-            options={{ 
-              headerShown: false,
-            }}
-            component={Login}
-            />
-            <Stack.Screen 
-            name='Dashboard' 
-            options={{ 
-              headerShown: false,
-            }}
-            component={Dashboard}
-            />
+        <Stack.Screen
+          name='Login'
+          options={{
+            headerShown: false
+          }}
+          component={Login}
+        />
+        <Stack.Screen
+          name='Dashboard'
+          options={{
+            headerShown: false
+          }}
+          component={Dashboard}
+        />
+        <Stack.Screen
+          name='AddCatch'
+          options={{
+            headerShown: false
+          }}
+          component={AddCatch}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-  
-  
-  
-          
-
