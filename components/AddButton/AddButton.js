1d30067d70e3c2e2e3e4292e852fbd7b914bd103
Banjamin Hatch <banjaminhatch@gmail.com>
2023-10-20 23:03:37 -0600

    Adds animations to action buttons

--- components/AddButton/AddButton.js
+++ components/AddButton/AddButton.js
@@ -1,76 +1,115 @@
-import { View, TouchableOpacity, StyleSheet, Animated, Text } from "react-native";
-import { styles } from './AddButtonStylesheet'
-import { useState } from "react";
-import Icon from 'react-native-vector-icons/FontAwesome'
-import { useNavigation } from '@react-navigation/native';
+import { View, TouchableOpacity, StyleSheet, Animated, Text, ImageBackground, Easing} from "react-native";
+import { styles } from "./AddButtonStylesheet";
+import { useState, useEffect} from "react";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { useNavigation } from "@react-navigation/native";
+import fishIcon from './fishIconNoBack.png'
 
+export default function AddButton({ toggleForm, tabPressed, setTabPressed }) {
+	const navigation = useNavigation();
 
+	const [addFish] = useState(new Animated.Value(40));
+	const [addLure] = useState(new Animated.Value(40));
+	const [opacity] = useState(new Animated.Value(0));
 
-export default function AddButton({toggleForm}){
-    const navigation = useNavigation();
+	const [isMenuOpen, setMenuOpen] = useState(false);
 
-    const [addFish] = useState(new Animated.Value(40));
-    const [addLure] = useState(new Animated.Value(40));
+	function navigateToLure() {
+		navigation.navigate("AddLure");
+	}
 
-    const [isMenuOpen, setMenuOpen] = useState(false);
+	const openMenu = () => {
+		setMenuOpen(true);
+		Animated.timing(addFish, {
+			toValue: 130,
+			duration: 400,
+			useNativeDriver: false,
+		}).start();
+		Animated.timing(addLure, {
+			toValue: 200,
+			duration: 400,
+			useNativeDriver: false,
+		}).start();
 
-    function navigateToLure(){
-        navigation.navigate('AddLure')
-    }
+		Animated.timing(opacity, {
+      toValue: 1, 
+      duration: 500,
+      easing: Easing.linear,
+      useNativeDriver: false, 
+    }).start();
+	};
 
-    const openMenu = () =>{
-        setMenuOpen(true);
-        Animated.timing(addFish, {
-            toValue: 130,
-            duration: 200,
-            useNativeDriver: false,
-        }).start()
-        Animated.timing(addLure, {
-            toValue: 200,
-            duration: 200,
-            useNativeDriver: false,
-        }).start()
-    }
+	const closeMenu = () => {
+		setMenuOpen(false);
+		Animated.timing(addFish, {
+			toValue: 40,
+			duration: 800,
+			useNativeDriver: false,
+		}).start();
+		Animated.timing(addLure, {
+			toValue: 40,
+			duration: 800,
+			useNativeDriver: false,
+		}).start();
+		
+		Animated.timing(opacity, {
+      toValue: 0, 
+      duration: 400,
+      easing: Easing.linear, 
+      useNativeDriver: false, 
+    }).start();
+	};
 
-    const closeMenu = () =>{
-        setMenuOpen(false);
-        Animated.timing(addFish, {
-            toValue: 40,
-            duration: 200,
-            useNativeDriver: false,
-        }).start()
-        Animated.timing(addLure, {
-            toValue: 40,
-            duration: 200,
-            useNativeDriver: false,
-        }).start()
-    }
+	useEffect(() => {
+		if(tabPressed){
+			closeMenu();
+			setTabPressed(false);
+		}
+	},[tabPressed])
 
-    return (
-        <View style={styles.mainContainer}>
-            <Animated.View style={[styles.buttonContainer, {bottom: addFish, opacity : isMenuOpen ? 1 : 0}]}>
-                <TouchableOpacity onPress={() => toggleForm()}>
-                    <Icon testID='fish-fins-icon' name='book' size={35} color='white'/>
-                </TouchableOpacity>
-										{/* <Text style={styles.buttonText}>Add catch</Text> */}
-            </Animated.View>
-            <Animated.View style={[styles.buttonContainer, {bottom: addLure, opacity : isMenuOpen ? 1 : 0}]}>
-                <TouchableOpacity onPress={() => navigateToLure()}>
-                    <Icon testID='lure-icon' name='superpowers' size={35} color='white'/>
-										{/* <Text style={styles.buttonText}>Add lure</Text> */}
-                </TouchableOpacity>
-            </Animated.View>
-            
-            <TouchableOpacity
-            testID='add-button'
-            style={styles.circleButton}
-            onPress={() => {
-                isMenuOpen === false ? openMenu() : closeMenu()
-            }
-            }
-            >
-                <Icon testID='plus-icon' name={isMenuOpen ? 'minus' : 'plus'} size={40} color='white'></Icon>
-            </TouchableOpacity>
-        </View>
-    )
+	return (
+		<View style={styles.mainContainer}>
+			<Animated.View style={[styles.addButtons, { bottom: addFish , opacity: opacity}]}>
+				<ImageBackground source={fishIcon} style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+				<TouchableOpacity
+					onPress={() => {
+						closeMenu();
+						toggleForm();
+					}}
+				>
+					<Text style={styles.buttonText}>Add Catch</Text>
+					{/* <Icon testID="fish-fins-icon" name="book" size={35} color="white" /> */}
+				</TouchableOpacity>
+				</ImageBackground>
+			</Animated.View>
+			<Animated.View style={[styles.addButtons, { bottom: addLure , opacity: opacity }]}>
+			<ImageBackground source={fishIcon} style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+				<TouchableOpacity
+					onPress={() => {
+						closeMenu();
+						navigateToLure();
+					}}
+				>
+					<Text style={styles.buttonText} >Add Lure</Text>
+					{/* <Icon testID="lure-icon" name="superpowers" size={35} color="white" /> */}
+				</TouchableOpacity>
+				</ImageBackground>
+			</Animated.View>
+
+			<TouchableOpacity
+				testID="add-button"
+				style={styles.circleButton}
+				onPress={() => {
+					isMenuOpen === false ? openMenu() : closeMenu();
+				}}
+			>
+				<Icon
+					testID="plus-icon"
+					name={isMenuOpen ? "minus" : "plus"}
+					size={40}
+					color="white"
+				></Icon>
+			</TouchableOpacity>
+		</View>
+	);
 }
--- components/AddButton/AddButtonStylesheet.js
+++ components/AddButton/AddButtonStylesheet.js
@@ -1,26 +1,47 @@
 import { StyleSheet } from "react-native";
 
-
 export const styles = StyleSheet.create({
-    mainContainer:{
-        flex: 1,
-        height: '100%',
-        width: '100%',
-        borderRadius: 30,
-    },
-    
-    
-    circleButton: {
-        backgroundColor: 'black',
-        width: 60,
-        height: 60,
-        position: 'absolute',
-        bottom: 40,
-        right: 190,
-        borderRadius: 30,
-        borderWidth: 2,
-        borderColor: 'white',
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
-})
+	mainContainer: {
+		flex: 1,
+		height: "100%",
+		width: "100%",
+		borderRadius: 30,
+	},
+
+	circleButton: {
+		backgroundColor: "black",
+		width: 60,
+		height: 60,
+		position: "absolute",
+		bottom: 40,
+		right: 190,
+		borderRadius: 30,
+		borderWidth: 2,
+		borderColor: "white",
+		alignItems: "center",
+		justifyContent: "center",
+		flex: 1,
+	},
+
+	addButtons: {
+		backgroundColor: "black",
+		width: 90,
+		height: 60,
+		position: "absolute",
+		bottom: 40,
+		right: 175,
+		borderRadius: 30,
+		alignItems: "center",
+		justifyContent: "center",
+		flex: 1,
+		shadowColor: "rgba(0, 0, 0, 0.8)",
+		shadowOffset: { width: 0, height: 6 },
+		shadowOpacity: .9,
+		shadowRadius: 5,
+	},
+
+	buttonText: {
+		fontSize: 15,
+		color: "white",
+	},
+});
