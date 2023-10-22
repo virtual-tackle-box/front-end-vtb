84223c9452a63ecb0091c77c27dc0f364c446023
Joshua Bennett <joshbennett793@yahoo.com>
2023-10-22 13:03:50 -0500

    Validate CatchLog props

--- components/Books/CatchLog/CatchLog.js
+++ components/Books/CatchLog/CatchLog.js
@@ -12,6 +12,8 @@ import { useEffect, useState } from 'react';
 import { deleteCatch, getCatches } from '../../../fetchCalls';
 import { useUserContext } from '../../UserContext/UserContext';
 
+import PropTypes from 'prop-types';
+
 import { CatchLogStylesheet as styles } from './CatchLogStylesheet';
 
 export default function CatchLog() {
@@ -93,3 +95,7 @@ export default function CatchLog() {
     </SafeAreaView>
   );
 }
+
+useUserContext.propTypes = {
+  userID: PropTypes.string.isRequired
+};
