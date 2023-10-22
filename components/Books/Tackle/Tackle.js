795af97bf5624090cefe43a5a2f4da3344bdd147
Banjamin Hatch <banjaminhatch@gmail.com>
2023-10-21 18:01:09 -0600

    Creates context provider and passes context

--- components/Books/Tackle/Tackle.js
+++ components/Books/Tackle/Tackle.js
@@ -9,26 +9,38 @@ import { Header } from 'react-native-elements';
 import { useNavigation } from '@react-navigation/native';
 import { useEffect, useState } from 'react';
 import { getLures } from '../../../fetchCalls';
+import { useUserContext } from "../../UserContext/UserContext"
+import { deleteLure } from '../../../fetchCalls';
 
 import { TackleStylesheet as styles } from './TackleStylesheet';
 
 export default function CatchLog() {
   const [lures, setLures] = useState([]);
-
+  const {userID} = useUserContext();
   const navigation = useNavigation();
 
-  useEffect(() => {
-    async function fetchLures() {
-      const lures = await getLures();
+  
+  async function fetchLures() {
+      const lures = await getLures(userID);
       setLures(lures.data);
     }
 
-    fetchLures();
-  }, []);
+  
+  
 
   function navToBooks() {
     navigation.navigate('book');
   }
+
+  async function removeLure(id){
+    const deletedLure = await deleteLure(userID,id);
+    fetchLures();
+  }
+
+  useEffect(() => {
+    fetchLures();
+  })
+
 
   const lureCards = lures.map(lure => {
     const { id } = lure;
@@ -36,6 +48,8 @@ export default function CatchLog() {
 
     return (
       <View style={styles.lureCard} key={id} testID={`${id}`}>
+        <TouchableOpacity onPress={() => removeLure(id) }>
+          <Text>X</Text></TouchableOpacity>
         <View>
           <Text>Brand: {brand}</Text>
           <Text>Type: {variety}</Text>
--- components/UserContext/UserContext.js
+++ components/UserContext/UserContext.js
@@ -1,29 +1,24 @@
-import { createContext, useContext, useReducer, useState } from 'react';
+import { createContext, useContext, useReducer, useState } from "react";
 
 const UserContext = createContext();
 
+export function UserContextProvider({ children }) {
+	const [userID, setUserID] = useState("");
 
+	const value = {
+		userID,
+		setUserID,
+	};
 
-export default function UserContextProvider({children}){
-    const [userID, setUserID] = useState('');
-
-
-    const value = {
-        userID,
-        setUserID,
-    }
-
-    return (
-        <UserContext.Provider value={value}>{children}</UserContext.Provider>
-    )
+	return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 }
 
-
-
-export function useUserContext({children}) {
-    const userData = useContext(UserContext)
-    if(!userData){
-        throw new Error('useUserContext can only be used within UserContextProvider')
-    }
-    return userData;
+export function useUserContext() {
+	const userData = useContext(UserContext);
+	if (!userData) {
+		throw new Error(
+			"useUserContext can only be used within UserContextProvider"
+		);
+	}
+	return userData;
 }
