1f335a75b335853d49b6ee1d2e081144b67d85ac
Banjamin Hatch <banjaminhatch@gmail.com>
2023-10-22 20:07:13 -0600

    Adds other icons for book page

--- components/Books/BooksDashStylesheet.js
+++ components/Books/BooksDashStylesheet.js
@@ -6,15 +6,39 @@ export const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: 'white'
+        backgroundColor: '#5A5A5A'
+        
     },
 
     dataContainer: {
-        height: '30%',
+        
+        height: '20%',
         width: '80%',
-        // backgroundColor: 'green',
-        justifyContent: 'flex-end',
+        backgroundColor: 'black',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
         alignItems: 'center',
         margin: 10,
+        borderWidth: 2,
+    },
+
+    imageContainer:{
+        width: 100,
+        height: 100,
+    },
+
+    images:{
+        width: 100,
+        height: 100,
+    },
+
+    textContainer:{
+        width: '40%'
+    },
+
+    text:{
+        fontSize: 35,
+        color: 'silver',
     }
+
 })
--- components/Books/BooksDashboard.js
+++ components/Books/BooksDashboard.js
@@ -1,69 +1,73 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import {
-  View,
-  Text,
-  Animated,
-  Touchable,
-  TouchableOpacity,
-  ImageBackground,
-} from 'react-native';
-import { styles } from './BooksDashStylesheet';
-import { useIsFocused, useNavigation } from '@react-navigation/native';
-import tackleIcon from './tackle-boxicon.png'
+	View,
+	Text,
+	Animated,
+	Touchable,
+	TouchableOpacity,
+	ImageBackground,
+	Image,
+} from "react-native";
+import { styles } from "./BooksDashStylesheet";
+import { useIsFocused, useNavigation } from "@react-navigation/native";
+import tackleIcon from "./tackle-boxicon.png";
+import bookIcon from './open-book.png'
 
 export default function BooksDashboard() {
-  const navigation = useNavigation();
-  const isFocused = useIsFocused();
-  const [slideInAnim] = useState(new Animated.Value(0));
+	const navigation = useNavigation();
+	const isFocused = useIsFocused();
+	const [slideInAnim] = useState(new Animated.Value(0));
 
-  useEffect(() => {
-    if (isFocused) {
-      Animated.timing(slideInAnim, {
-        toValue: 1,
-        duration: 400,
-        useNativeDriver: true
-      }).start();
-    } else {
-      slideInAnim.setValue(0);
-    }
-  }, [isFocused]);
+	useEffect(() => {
+		if (isFocused) {
+			Animated.timing(slideInAnim, {
+				toValue: 1,
+				duration: 400,
+				useNativeDriver: true,
+			}).start();
+		} else {
+			slideInAnim.setValue(0);
+		}
+	}, [isFocused]);
 
-  const slideInStyle = {
-    transform: [
-      {
-        translateX: slideInAnim.interpolate({
-          inputRange: [0, 1],
-          outputRange: [-300, 0]
-        })
-      }
-    ]
-  };
+	const slideInStyle = {
+		transform: [
+			{
+				translateX: slideInAnim.interpolate({
+					inputRange: [0, 1],
+					outputRange: [-300, 0],
+				}),
+			},
+		],
+	};
 
-  function navToCatchLog() {
-    navigation.navigate('CatchLog');
-  }
+	function navToCatchLog() {
+		navigation.navigate("CatchLog");
+	}
 
-  function navToTackle() {
-    navigation.navigate('Tackle');
-  }
+	function navToTackle() {
+		navigation.navigate("Tackle");
+	}
 
-  return (
-    <Animated.View style={[styles.container, slideInStyle]}>
-      <ImageBackground source={tackleIcon} style={{ flex: 0.4, justifyContent: "center", alignItems: "center", width: '90%', height: '90%'}} resizeMode='contain'>
-        <TouchableOpacity style={styles.dataContainer} onPress={navToTackle}>
-          <View style={styles.dataContainer}>
-            <Text>Lures</Text>
-          </View>
-        </TouchableOpacity>
-      </ImageBackground>
-      <ImageBackground source={tackleIcon} style={{ flex: 0.4, justifyContent: "center", alignItems: "center", width: '90%', height: '90%'}} resizeMode='contain'>
-      <TouchableOpacity style={styles.dataContainer} onPress={navToCatchLog}>
-        <View style={styles.dataContainer}>
-          <Text>Catch Log</Text>
-        </View>
-      </TouchableOpacity>
-      </ImageBackground>
-    </Animated.View>
-  );
-  
-  }
+	return (
+		<Animated.View style={[styles.container, slideInStyle]}>
+			<TouchableOpacity style={styles.dataContainer} onPress={navToTackle}>
+				<View style={styles.imageContainer}>
+					<Image style={styles.images} source={tackleIcon} />
+				</View>
+				<View style={styles.textContainer}>
+					<Text style={styles.text}>Lures</Text>
+				</View>
+			</TouchableOpacity>
+
+			<TouchableOpacity style={styles.dataContainer} onPress={navToCatchLog}>
+      <View style={styles.imageContainer}>
+					<Image style={styles.images} source={bookIcon} />
+				</View>
+				<View style={styles.textContainer}>
+					<Text style={styles.text}>Catch Log</Text>
+				</View>
+			</TouchableOpacity>
+		</Animated.View>
+	);
+}
