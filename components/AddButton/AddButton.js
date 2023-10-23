42f4bd63c3fb211cefd1d80b665b1a3251a0de93
Joshua Bennett <joshbennett793@yahoo.com>
2023-10-23 00:21:50 -0500

    Update AddButton prop validation

--- components/AddButton/AddButton.js
+++ components/AddButton/AddButton.js
@@ -25,8 +25,8 @@ export default function AddButton({ toggleForm, tabPressed, setTabPressed }) {
   const [addLure] = useState(new Animated.Value(40));
   const [opacity] = useState(new Animated.Value(0));
 
-	const [isMenuOpen, setMenuOpen] = useState(false);
-	const { showMarker, setShowMarker } = useUserContext();
+  const [isMenuOpen, setMenuOpen] = useState(false);
+  const { setShowMarker } = useUserContext();
 
   function navigateToLure() {
     navigation.navigate('AddLure');
@@ -45,113 +45,117 @@ export default function AddButton({ toggleForm, tabPressed, setTabPressed }) {
       useNativeDriver: false
     }).start();
 
-		Animated.timing(opacity, {
-			toValue: 1,
-			duration: 500,
-			easing: Easing.linear,
-			useNativeDriver: false,
-		}).start();
-	};
+    Animated.timing(opacity, {
+      toValue: 1,
+      duration: 500,
+      easing: Easing.linear,
+      useNativeDriver: false
+    }).start();
+  };
 
-	const closeMenu = () => {
-		setMenuOpen(false);
-		Animated.timing(addFish, {
-			toValue: 40,
-			duration: 800,
-			useNativeDriver: false,
-		}).start();
-		Animated.timing(addLure, {
-			toValue: 40,
-			duration: 800,
-			useNativeDriver: false,
-		}).start();
+  const closeMenu = () => {
+    setMenuOpen(false);
+    Animated.timing(addFish, {
+      toValue: 40,
+      duration: 800,
+      useNativeDriver: false
+    }).start();
+    Animated.timing(addLure, {
+      toValue: 40,
+      duration: 800,
+      useNativeDriver: false
+    }).start();
 
-		Animated.timing(opacity, {
-			toValue: 0,
-			duration: 400,
-			easing: Easing.linear,
-			useNativeDriver: false,
-		}).start();
-	};
+    Animated.timing(opacity, {
+      toValue: 0,
+      duration: 400,
+      easing: Easing.linear,
+      useNativeDriver: false
+    }).start();
+  };
 
-	useEffect(() => {
-		if (tabPressed) {
-			closeMenu();
-			setTabPressed(false);
-		}
-	}, [tabPressed]);
+  useEffect(() => {
+    if (tabPressed) {
+      closeMenu();
+      setTabPressed(false);
+    }
+  }, [tabPressed]);
 
-	return (
-		<View style={styles.mainContainer}>
-			<Animated.View
-				style={[styles.addButtons, { bottom: addFish, opacity: opacity }]}
-			>
-				<ImageBackground
-					source={fishIcon}
-					style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-				>
-					<TouchableOpacity
-						onPress={() => {
-							closeMenu();
-							toggleForm();
-						}}
-					>
-						<Text testID="fish-fins-icon" style={styles.buttonText}>
-							Add Catch
-						</Text>
-						{/* <Icon testID="fish-fins-icon" name="book" size={35} color="white" /> */}
-					</TouchableOpacity>
-				</ImageBackground>
-			</Animated.View>
-			<Animated.View
-				style={[styles.addButtons, { bottom: addLure, opacity: opacity }]}
-			>
-				<ImageBackground
-					source={fishIcon}
-					style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-				>
-					<TouchableOpacity
-						onPress={() => {
-							closeMenu();
-							navigateToLure();
-						}}
-					>
-						<Text testID="lure-icon" style={styles.buttonText}>
-							Add Lure
-						</Text>
-						{/* <Icon testID="lure-icon" name="superpowers" size={35} color="white" /> */}
-					</TouchableOpacity>
-				</ImageBackground>
-			</Animated.View>
+  return (
+    <View style={styles.mainContainer}>
+      <Animated.View
+        style={[styles.addButtons, { bottom: addFish, opacity: opacity }]}
+      >
+        <ImageBackground
+          source={fishIcon}
+          style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+        >
+          <TouchableOpacity
+            onPress={() => {
+              closeMenu();
+              toggleForm();
+            }}
+          >
+            <Text testID='fish-fins-icon' style={styles.buttonText}>
+              Add Catch
+            </Text>
+            {/* <Icon testID="fish-fins-icon" name="book" size={35} color="white" /> */}
+          </TouchableOpacity>
+        </ImageBackground>
+      </Animated.View>
+      <Animated.View
+        style={[styles.addButtons, { bottom: addLure, opacity: opacity }]}
+      >
+        <ImageBackground
+          source={fishIcon}
+          style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+        >
+          <TouchableOpacity
+            onPress={() => {
+              closeMenu();
+              navigateToLure();
+            }}
+          >
+            <Text testID='lure-icon' style={styles.buttonText}>
+              Add Lure
+            </Text>
+            {/* <Icon testID="lure-icon" name="superpowers" size={35} color="white" /> */}
+          </TouchableOpacity>
+        </ImageBackground>
+      </Animated.View>
 
-			<TouchableOpacity
-				testID="add-button"
-				style={styles.circleButton}
-				onPress={() => {
-					isMenuOpen === false
-						? (() => {
-								setShowMarker(true);
-								openMenu();
-						  })()
-						: (() => {
-								closeMenu();
-								setShowMarker(false);
-						  })();
-				}}
-			>
-				<Icon
-					testID="plus-icon"
-					name={isMenuOpen ? "minus" : "plus"}
-					size={40}
-					color="white"
-				></Icon>
-			</TouchableOpacity>
-		</View>
-	);
+      <TouchableOpacity
+        testID='add-button'
+        style={styles.circleButton}
+        onPress={() => {
+          isMenuOpen === false
+            ? (() => {
+                setShowMarker(true);
+                openMenu();
+              })()
+            : (() => {
+                closeMenu();
+                setShowMarker(false);
+              })();
+        }}
+      >
+        <Icon
+          testID='plus-icon'
+          name={isMenuOpen ? 'minus' : 'plus'}
+          size={40}
+          color='white'
+        ></Icon>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 AddButton.propTypes = {
   toggleForm: PropTypes.func.isRequired,
   tabPressed: PropTypes.bool.isRequired,
   setTabPressed: PropTypes.func.isRequired
+};
+
+useUserContext.propTypes = {
+  setShowMarker: PropTypes.func.isRequired
 };
