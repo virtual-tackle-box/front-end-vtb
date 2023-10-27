e4fe61824acb0f1dc4fc8441fed1b3960d3972ff
Banjamin Hatch <banjaminhatch@gmail.com>
2023-10-26 21:37:43 -0600

    Adds activityindicator to catch modal

--- components/AddCatch/CatchModal/CatchModal.js
+++ components/AddCatch/CatchModal/CatchModal.js
@@ -1,5 +1,5 @@
 import {useState, useEffect} from 'react';
-import { View, Text, Modal, TouchableOpacity } from 'react-native';
+import { View, Text, Modal, TouchableOpacity, ActivityIndicator } from 'react-native';
 import {styles} from './CatchModalStylesheet'
 
 
@@ -16,13 +16,15 @@ export default function CatchModal({visible, setModalVisible}){
         visible={visible}
         >
             <View  style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0,0,0,.7)'}}>
-                <TouchableOpacity onPress={() => setModalVisible(false)}>
+                {/* <TouchableOpacity onPress={() => setModalVisible(false)}>
                     <View style={styles.closeButton}>
                     <Text style={styles.closeText}>X</Text>
                     </View>
-                </TouchableOpacity>
-                <View style={{backgroundColor: 'black', padding: 20, borderRadius: 10, width: '50%',justifyContent: 'center', alignItems: 'center'}}>
-                    <Text style={{ color: 'white'}}>Adding catch...</Text>
+                </TouchableOpacity> */}
+                {/* <ActivityIndicator/> */}
+                <View style={{backgroundColor: 'rgba(0,0,0,.4)', padding: 20, borderRadius: 10, width: '50%',justifyContent: 'center', alignItems: 'center'}}>
+                <ActivityIndicator/>
+                    <Text style={{ color: 'white', marginTop: 10}}>Adding catch</Text>
                 </View>
             </View>
         </Modal>
